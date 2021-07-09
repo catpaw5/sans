d85cefa4e3e85093d79dd80c11b4045247b06926
catpaw5 <87186250+catpaw5@users.noreply.github.com>
2021-07-09 21:26:45 +0900

    ㅋㅋ
    ㅁㅁ

---
+++ Assets/CharacterMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class CharacterMove : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody characterRigidbody;
+ 
+    void Start(){
+        characterRigidbody = GetComponent<Rigidbody>();
+    }
+ 
+    void Update(){
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            characterRigidbody.AddForce(-speed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)){
+           characterRigidbody.AddForce(speed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow)){
+            characterRigidbody.AddForce(0, 0, speed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow)){
+            characterRigidbody.AddForce(0, 0, -speed);
+        }
+    }
+}
+ 
